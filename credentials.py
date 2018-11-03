0a0a5684dd00fb7f70a2de3e0ff5ff9b371134d1
Sam Kasyoki <samkasyoki@gmail.com>
2018-11-03 13:47:19 +0300

    enable class Credentials to generate instances of credentials

---
+++ credentials.py
@@ -0,0 +1,15 @@
+class Credentials:
+    """
+      Class that creates instances of credentials.
+    """
+
+    def __init__(self, username, password):
+        """
+          Defines properties for our objects.
+
+          Args:
+            username: new username.
+            password: new password.
+        """
+        self.username = username
+        self.password = password
