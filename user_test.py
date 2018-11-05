f50b87c2f9c6e5eb07579aab693fcba1247349e4
Sam Kasyoki <samkasyoki@gmail.com>
2018-11-05 21:11:50 +0300

    create a test to see if a user object can be saved to user_list

--- user_test.py
+++ user_test.py
@@ -25,6 +25,20 @@ class TestUser(TestCase):
         self.assertEqual(self.new_user.username, 'sam10105')
         self.assertEqual(self.new_user.password, 'samisbae')
 
+    def test_save_user(self):
+        """
+          Test case to see if objects are being stored on user_list.
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
+
+    def test_user_exists(self):
+        """
+          Test case to see if a specified user exists.
+        """
+        self.new_user.save_user()
+        self.assertTrue(User.user_exists('sam10105'))
+
 
 if __name__ == '__main__':
     main()
