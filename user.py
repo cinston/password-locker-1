f3d51084953877b79cc0dac43678aeff22ea2f7a
Sam Kasyoki <samkasyoki@gmail.com>
2018-11-05 21:10:10 +0300

    User class can now save users

--- user.py
+++ user.py
@@ -3,6 +3,8 @@ class User:
       A class that generates new instances of users.
     """
 
+    user_list = []
+
     def __init__(self, first_name, last_name, username, password):
         """
           Defines properties for our objects.
@@ -18,3 +20,26 @@ class User:
         self.last_name = last_name
         self.username = username
         self.password = password
+
+    def save_user(self):
+        """
+          Method that saves objects to user_list.
+        """
+        self.user_list.append(self)
+
+    @classmethod
+    def user_exists(cls, logged_user):
+        """
+        Method that checks is a user exists from the user list
+
+        args:
+            logged_user:username to search if the user exists
+
+        returns:
+            boolean:true or false depending on the condition
+        """
+        for user in cls.user_list:
+            if user.username == logged_user:
+                return True
+
+        return False
